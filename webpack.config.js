b37fb36521dc3506372ba09962a2758e42865e07
Khandaker Shahriar Shamit <shahriar.shamit@gmail.com>
2021-10-19 18:49:02 +0600

    Coding Done

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve("docs"),
+        publicPath: "/",
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         "style-loader",
+            //         "css-loader",
+            //         "sass-loader"
+            //     ],
+            // },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "index.html"
+        }),
+    ]
+}
